1b482c9b94fdf06484a7b7326cb83aceeaea6849
Smirnov EV <knyazz@gmail.com>
2015-01-29 21:08:12 +0400

    fix relations set adds

--- hockeyapp/managers/match.py
+++ hockeyapp/managers/match.py
@@ -176,11 +176,12 @@ class MatchManager(ManagerMixin, models.Manager):
         club_model = get_model(CURRENT_APP, 'club')
         _club, _crt = club_model.objects.get_or_create(**kwargs)
         if _players:
-            _players = [self._get_player(   p.get('khl_id'),
-                                            p.get('ru_fio'),
-                                        ) for p in _players
-            ]
-            _club.players = list(_players)
+            _players = list([self._get_player(  p.get('khl_id'),
+                                                p.get('ru_fio'),
+                                            ) for p in _players
+            ])
+            if _players != list(_club.players.all()):
+                _club.players = list(_players)
         if _region:
             model = get_model('addresses', 'Address')
             _region, _crt = model.objects.get_or_create(ru_title=_region)
