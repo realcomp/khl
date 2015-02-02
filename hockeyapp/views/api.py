61b93386aeb31ab7929a46cb5ce5bb224d93b723
Okami <okami@fuzetsu.info>
2015-02-02 17:30:35 +0300

    fix #92

--- hockeyapp/views/api.py
+++ hockeyapp/views/api.py
@@ -143,35 +143,9 @@ class PlayerCardIndicators(generics.ListAPIView):
         qs = qs.filter(clubplayer__in=clubplayers)
 
         if self.request.GET.get('group_by') == 'season':
-            seasons = (
-                Season.objects
-                .filter(pk__in=clubplayers.values_list('season_id'))
-                .order_by('start_date'))
-            qss = []
-            for season in seasons:
-                season_qs = qs.filter(clubplayer__season=season)
-                season_qs.date = None
-                season_qs.season = season
-                season_qs._aggregate = self._get_aggregate(season_qs)
-                qss.append(season_qs)
-            return qss
+            return qs.group_by_season()
         else:  # group by month (by default)
-            min_max = qs.aggregate(Min('match__date'), Max('match__date'))
-            start_date = min_max.get('match__date__min')
-            end_date = min_max.get('match__date__max')
-            if start_date and end_date:
-                qss = []
-                dates = list(month_range(start_date, end_date))
-                for i in range(len(dates) - 1):
-                    month_qs = qs.filter(
-                        match__date__gt=dates[i],
-                        match__date__lte=dates[i + 1])
-                    month_qs.date = dates[i]
-                    month_qs.season = None
-                    month_qs._aggregate = self._get_aggregate(month_qs)
-                    qss.append(month_qs)
-                return qss
-        return []
+            return qs.group_by_month()
 
 
 class LeagueList(generics.ListAPIView):
