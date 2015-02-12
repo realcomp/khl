07e36873cec5aee1e7c76f0d81f7ffeee96d0bdb
amorphousxd <amorphousxd@gmail.com>
2015-02-12 18:00:05 +0300

    scroll player initial, some refactor

--- base/static/js/sportomatics.js
+++ base/static/js/sportomatics.js
@@ -89,8 +89,16 @@ $(function() {
     var top = null;
     var topSecondary = null;
     var topThird = null;
+    var topPlayer = null;
     var breadcrumbWidth = null;
     var teamNameWidth = null;
+    var teamLogoMargin = null;
+    var playerNameWidth = null;
+    var scrolledAfterPlayer = false;
+    var scrolledAfterBreadcrumbs = false;
+    var scrolledAfterTeamInfo = false;
+    var scrolledAfterGreenMenu = false;
+    //jquery elements
     if($('.breadcrumbs').length){
         top = $('.breadcrumbs').offset().top;
         if($('.breadcrumb').length)
@@ -103,21 +111,28 @@ $(function() {
     if($('.page-menu').length){
         topThird = $('.page-menu').offset().top - 40;
     }
+    if($('.player-card-block').length){
+        topPlayer = $('.breadcrumbs').next().offset().top + 120;
+        playerNameWidth = $('#player-card-name').textWidth();
+    }
+    if(breadcrumbWidth && teamNameWidth){
+        teamLogoMargin = (1000 - breadcrumbWidth*2 - (25+teamNameWidth))/2;
+    }
     $(window).scroll(function(event) {
         var y = $(window).scrollTop();
         if (top && y >= top) {
             if($('.team-info').length) {
-                $('.team-info').css('margin-top', '52px');
+                $('.team-info').css('margin-top', '52px'); // club page
             } else {
-                if($('.player-card-block').length){
+                if($('.player-card-block').length){ // players-page
                     if($('.breadcrumbs').length){
                         $('.breadcrumbs').next().css('margin-top', '30px')
                     }
-                } else ($('.page-container').css('margin-top', '42px'))
+                } else ($('.page-container').css('margin-top', '42px')) // clubs page
             }
-            $('.breadcrumbs').addClass('fixed');//.css('border-bottom', '1px solid #f2f2f2');
+            $('.breadcrumbs').addClass('fixed');
         } else if(top && y < top) {
-            $('.breadcrumbs').removeClass('fixed');//.css('border-bottom', 'none');
+            $('.breadcrumbs').removeClass('fixed');
             if($('.team-info').length) {
                 $('.team-info').css('margin-top', '0px');
             } else {
@@ -128,48 +143,54 @@ $(function() {
                 } else ($('.page-container').css('margin-top', '0px'))
             }
         }
+        //player card
+        if(topPlayer && y >= topPlayer && !scrolledAfterPlayer){
+            $('.breadcrumb').after($('#player-card-name').addClass('inline-block').css('margin-left', (1000 - breadcrumbWidth*2 - playerNameWidth)/2 + 'px', 'important'));
+            //$('.player-card-block > img').addClass('clipped-img');
+            //$('.breadcrumbs').next().next().css('margin-top', '180px');
+            $('.search-block').after($('.page-menu').css('margin-left', '5px', 'important').css('margin-right', '5px', 'important'));
+            $('.page-container').css('margin-top', '62px');
+            scrolledAfterPlayer = true;
+        } else if (topPlayer && y < topPlayer && scrolledAfterPlayer){
+            $('#player-card-amplua').before($('#player-card-name').removeClass('inline-block').css('margin-left', 0 + 'px', 'important'));
+            $('.player-card-block > img').removeClass('clipped-img');
+            $('.page-inner-container').before($('.page-menu').removeClass('fixed').css('margin-left', '0px', 'important').css('margin-right', '0px', 'important'));
+            $('.page-container').css('margin-top', '0px');
+            scrolledAfterPlayer = false;
+        }
+        //team card
         if (topSecondary && y >= topSecondary){
             $('.team-info').css('margin-top', '106px');
             $('.my-team-btn').css('margin-top', '4px');
-            $('.breadcrumb').after($('#team-logo'));
-            $("#team-logo").after($('#team-name'));
+            $('.breadcrumb').after($('#team-logo')).addClass('inline-block breadcrumb-inner');
+            $("#team-logo").after($('#team-name')).addClass('team-logo-inner inline-block').css('margin-left', teamLogoMargin + 'px', 'important');
             $('#team-name').addClass('team-name-inner inline-block');
-            $('.breadcrumb').addClass('inline-block breadcrumb-inner');
-            $('#team-logo').addClass('team-logo-inner inline-block');
-            $('#team-logo').css('margin-left', (1000 - breadcrumbWidth*2 - (25+teamNameWidth))/2 + 'px', 'important');
         } else if (topSecondary && y < topSecondary){
-            $('.my-team-btn').after($('#team-logo'));
-            $('.my-team-btn').css('margin-top', '10px');
-            $('#team-logo').after($('#team-name'));
-            $('#team-logo').css('margin-left', '0px');
+            $('.my-team-btn').after($('#team-logo')).css('margin-top', '10px');
             $('.breadcrumb').removeClass('inline-block breadcrumb-inner');
-            $('#team-logo').removeClass('team-logo-inner inline-block');
+            $('#team-logo').after($('#team-name')).removeClass('team-logo-inner inline-block').css('margin-left', '0px');
             $('#team-name').removeClass('team-name-inner inline-block');
         }
+        //green menu
         if (topThird && y >= topThird){
-            $('.team-info').css('margin-top', '150px', 'important')
-            $('.search-block').after($('.page-menu'));
-            $('.page-menu').css('margin-left', '5px', 'important');
-            $('.page-menu').css('margin-right', '5px', 'important');
+            if($('.team-info').length)
+            $('.team-info').css('margin-top', '150px', 'important');
+            if(!$('#player-card-block').length)
+            $('.search-block').after($('.page-menu').css('margin-left', '5px', 'important').css('margin-right', '5px', 'important'));
         } else if (topThird && y < topThird){
-            $('.page-menu').removeClass('fixed');
-            $('.page-inner-container').before($('.page-menu'));
-            $('.page-menu').css('margin-left', '0px', 'important');
-            $('.page-menu').css('margin-right', '0px', 'important');
+            if(!$('#player-card-block').length)
+            $('.page-inner-container').before($('.page-menu').removeClass('fixed').css('margin-left', '0px', 'important').css('margin-right', '0px', 'important'));
         }
     });
     var y = $(window).scrollTop();
+    //green menu
     if (topThird && y >= topThird){
         $('.team-info').css('margin-top', '150px', 'important')
-        $('.search-block').after($('.page-menu'));
-        $('.page-menu').css('margin-left', '5px', 'important');
-        $('.page-menu').css('margin-right', '5px', 'important');
+        $('.search-block').after($('.page-menu').css('margin-left', '5px', 'important').css('margin-right', '5px', 'important'));
     } else if (topThird && y < topThird){
-        $('.page-menu').removeClass('fixed');
-        $('.page-inner-container').before($('.page-menu'));
-        $('.page-menu').css('margin-left', '0px', 'important');
-        $('.page-menu').css('margin-right', '0px', 'important');
+        $('.page-inner-container').before($('.page-menu').removeClass('fixed').css('margin-left', '0px', 'important').css('margin-right', '0px', 'important'));
     }
+    //team card
     if (topSecondary && y >= topSecondary){
         $('.team-info').css('margin-top', '106px');
         $('.my-team-btn').css('margin-top', '4px');
@@ -182,10 +203,8 @@ $(function() {
     } else if (topSecondary && y < topSecondary){
         $('.my-team-btn').after($('#team-logo'));
         $('.my-team-btn').css('margin-top', '10px');
-        $('#team-logo').after($('#team-name'));
-        $('#team-logo').css('margin-left', '0px');
+        $('#team-logo').after($('#team-name')).removeClass('team-logo-inner inline-block').css('margin-left', '0px');
         $('.breadcrumb').removeClass('inline-block breadcrumb-inner');
-        $('#team-logo').removeClass('team-logo-inner inline-block');
         $('#team-name').removeClass('team-name-inner inline-block');
     }
     if (top && y >= top) {
@@ -217,3 +236,11 @@ $(function() {
         ($('.breadcrumb').first().find($('.section').last()).css('text-decoration', 'none'));
     }
 });*/
+$.fn.textWidth = function(){
+    var html_org = $(this).html();
+    var html_calc = '<span>' + html_org + '</span>';
+    $(this).html(html_calc);
+    var width = $(this).find('span:first').width();
+    $(this).html(html_org);
+    return width;
+};
