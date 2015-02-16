2fd18f823a56b9a31683aedcfc8049688e653448
Smirnov EV <knyazz@gmail.com>
2015-02-16 19:48:35 +0400

    fix celery raven logging

--- sportomatics/celery.py
+++ sportomatics/celery.py
@@ -14,7 +14,10 @@ class Celery(celery.Celery):
     def on_configure(self):
         import raven
         from raven.contrib.celery import register_signal, register_logger_signal
-        client = raven.Client()
+        if hasattr(settings, 'RAVEN_CONFIG'):
+            client = raven.Clientt(dsn=settings.RAVEN_CONFIG.get('dsn'))
+        else:
+            client = raven.Client()
         # register a custom filter to filter out duplicate logs
         register_logger_signal(client)
         # hook into the Celery error handler
