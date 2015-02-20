e8b7927dd59567f8a821b37b124cf513addc593b
Smirnov EV <knyazz@gmail.com>
2015-02-20 16:29:54 +0400

    fix #194

--- sportomatics/settings.py
+++ sportomatics/settings.py
@@ -112,11 +112,12 @@ CELERYBEAT_SCHEDULE = {
         'task': 'hockeyapp.tasks.periodic_get_matches',
         'schedule': local_celery_crontab(hour=1, minute=0),
     },
-    #'hockeyapp-periodic_get-clubs-instagram_pictures-every-day-midnight': {
-        #'task': 'hockeyapp.tasks._get_clubs_instagram_pictures',
-        #'schedule': local_celery_crontab(hour=0, minute=0),
-    #},
+    'hockeyapp-periodic_get-clubs-instagram_pictures-every-day-midnight': {
+        'task': 'hockeyapp.tasks._get_clubs_instagram_pictures',
+        'schedule': local_celery_crontab(hour=0, minute=0),
+    },
 }
+CELERY_IGNORE_RESULT = True
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
