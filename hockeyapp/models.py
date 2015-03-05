ea5f295677ce066f346598477b5c1607c28503e8
Smirnov EV <knyazz@gmail.com>
2015-03-05 14:08:35 +0400

    fix grb validator

--- hockeyapp/models.py
+++ hockeyapp/models.py
@@ -22,8 +22,8 @@ from .choices import CHALLENGE_TYPE
 from . import managers
 
 def rgb_validator(value):
-    if not re.match(r'^rgba\([0-9]+,[0-9]+,[0-9]+\,[0-9]+\.[0-9]+\)$', value):
-        raise ValidationError('Incorrect format. Expected `rgba(#,#,#, #opacity)`.')
+    if not re.match(r'^[0-9]+,[0-9]+,[0-9]+\$', value):
+        raise ValidationError('Incorrect format. Expected `#,#,#`.')
 
 
 class AbstractMan(LocaleAttrMixin, models.Model):
