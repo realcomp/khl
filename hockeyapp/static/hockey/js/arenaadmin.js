c7b4061fb0ae017609ab07ca64c6c0a5d3c77cc8
Smirnov EV <knyazz@gmail.com>
2015-03-12 14:03:37 +0400

    fix #94

--- hockeyapp/static/hockey/js/arenaadmin.js
+++ hockeyapp/static/hockey/js/arenaadmin.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         setTimeout(function () {
             var str = $('#id_coords').val();
             var res = str.match(/@[0-9]+.[0-9]+,[0-9]+.[0-9]+/);
-            if (res.length > 0) {
+            if (res && res.length > 0) {
                 res = res[0].slice(1);
                 $('#id_coords').val(res);
             }
