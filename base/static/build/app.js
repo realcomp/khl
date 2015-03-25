1fd853be1fcfa96d8055efec0a4191b47e3c601e
amorphousxd <amorphousxd@gmail.com>
2015-03-25 12:56:01 +0300

    #114 player partners add, styles

--- base/static/build/app.js
+++ base/static/build/app.js
@@ -3273,6 +3273,11 @@ angular.module('Sportomatics')
         $scope.go = function(path){
             window.open(path);
         }
+        $scope.limit = [4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4];
+        $scope.increaseLimit = function(index){
+            console.log(index);
+            $scope.limit[index] += 4;
+        }
         $scope.setPlaying = function(value){
             $scope.params['is_playing'] = 1;
             $scope.getPartners();
@@ -3281,10 +3286,13 @@ angular.module('Sportomatics')
             if(($scope.params && $scope.params.is_playing) || ($scope.params && $scope.params.rate_by)){
                 $scope.url += '?'+ $.param($scope.params)
             }
+            $scope.loader = true;
             $http.get($scope.url)
                 .success(function(data){
                     console.log(data)
-                    $scope.playersBySeasonCount = _.filter(_.sortBy(data, 'seasons_count').reverse(), function(el){ return el.seasons_count > 3});
+                    $scope.loader = false;
+                    $scope.playersBySeasonCount = _.filter(_.sortBy(data, 'seasons_count').reverse(), function(el){ return el.seasons_count > 0});
+
                 })
         };
         $scope.getPartners();
