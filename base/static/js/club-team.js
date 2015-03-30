19e67c72a7fc462b603a692512189f8247e57ed5
upteran <upteran@gmail.com>
2015-03-30 16:00:10 +0300

    edit menu

--- base/static/js/club-team.js
+++ base/static/js/club-team.js
@@ -3,3 +3,4 @@ $('.tab-photo-wrap .ui.button').click(function(){
    $('.active-photo-table').removeClass('active-photo-table').addClass('disactive-photo-table')
    $(this).removeClass('disactive-photo-table').addClass('active-photo-table')
    });
+});
