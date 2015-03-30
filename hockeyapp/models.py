25aa4de3fa1c3cd01915c370d16bd4237f3390aa
Evgenii Smirnov <knyazz@gmail.com>
2015-03-30 18:16:53 +0300

    fix #243

--- hockeyapp/models.py
+++ hockeyapp/models.py
@@ -1,7 +1,6 @@
 #coding: utf-8
 from __future__ import unicode_literals
 import datetime
-import itertools
 import re
 import urllib
 
@@ -10,7 +9,7 @@ from dateutil import relativedelta
 from django.core.exceptions import ValidationError
 from django.core.urlresolvers import reverse
 from django.db import models
-from django.db.models import F, Q, Avg, Sum
+from django.db.models import Q
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
