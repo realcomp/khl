e32996f49364f69f3e635ab0a53408f087718e37
Okami <okami@fuzetsu.info>
2015-03-30 18:08:05 +0300

    fix #146

--- hockeyapp/views/api.py
+++ hockeyapp/views/api.py
@@ -29,7 +29,7 @@ from ..serializers.clubs import (
 from ..serializers.events import EventSerializer
 from ..serializers.players import (
     PlayersSearchSerializer, ClubPlayerMatchSerilizer, PlayerNamesSerializer,
-    ClubTitlesSerializer)
+    ClubTitlesSerializer, ClubPlayerMatchPaginationSerilizer)
 from ..serializers.schedule import ScheduleSerializer
 from ..serializers.timeline import PlayerTimelineSerializer
 
@@ -78,6 +78,7 @@ class BestPlayer(PlayersSearch):
 class PlayerCardIndicators(generics.ListAPIView):
     queryset = ClubPlayerMatch.objects.all()
     paginate_by = 99999
+    pagination_serializer_class = ClubPlayerMatchPaginationSerilizer
     serializer_class = ClubPlayerMatchSerilizer
 
     def _get_aggregate(self, qs):
