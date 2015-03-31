3ccf7f674d58bb809818a4324bb7643937e7051f
Okami <okami@fuzetsu.info>
2015-03-31 13:37:39 +0300

    fix #247

--- sportomatics/urls.py
+++ sportomatics/urls.py
@@ -19,7 +19,7 @@ urlpatterns = patterns(
     url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^select2/', include('django_select2.urls')),
     url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', name='sitemap_xml'),
-    url(r'^ulogin/', include('django_ulogin.urls')),
+    url(r'', include('social_auth.urls')),
 )
 
 if 'autocomplete_light' in settings.INSTALLED_APPS:
