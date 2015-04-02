e157f8233e1b3ba9e4061f8dfcd5fdcc5223ee95
amorphousxd <amorphousxd@gmail.com>
2015-04-02 14:00:38 +0300

    minor fix

--- base/static/build/app.js
+++ base/static/build/app.js
@@ -1877,7 +1877,9 @@ angular.module('Sportomatics').factory('RadarChartFactory', function(ChartFactor
                     })
                 }
             }
+
             return deferred.promise;
+
         }; // createRadar
 
         this.draw = function(){
