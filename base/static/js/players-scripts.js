6e94caca293b762bb81194f97fe2c85d994c458c
Okami <okami@fuzetsu.info>
2015-04-07 12:08:12 +0300

    players search form

--- base/static/js/players-scripts.js
+++ base/static/js/players-scripts.js
@@ -1,53 +1,44 @@
- $(function () { 
- $('.ui.dropdown')
-  .dropdown();
-  $('.checkbox')
-  .checkbox(); 
-  $('#search-select')
-  .dropdown();
-  $('#checkCountry')
-  .checkbox();
-  $('#checkC')
-  .checkbox();
-  $( "#datepicker" ).datepicker();
-   
- $("#range").ionRangeSlider({
-            hide_min_max: true,
-            keyboard: true,
-            min: 15,
-            max: 45,
-            from: 28,
-            to: 32,
-            type: 'double',
-            step: 1,
-            grid: false
-        });
+$(function () {
+    $('.ui.dropdown').dropdown();
+    $('.checkbox').checkbox();
+    $('#search-select').dropdown();
+    $('#checkCountry').checkbox();
+    $('#checkC').checkbox();
+    $('#datepicker').datepicker();
 
-   
-      
- $("#rangeTwo").ionRangeSlider({
-            hide_min_max: true,
-            keyboard: true,
-            min: 0,
-            max: 100,
-            from: 40,
-            to: 80,
-            type: 'double',
-            step: 1,
-            grid: false
-        });
-
-$(function() {  
-   $('select').styler();  
-  });  
-
-(jQuery);
-jQuery(function(){
-        jQuery('.jq-select-multiple ul').jScrollPane({
-        verticalDragMinHeight: 20,
-        verticalDragMaxHeight: 20
-      });
+    $("#range").ionRangeSlider({
+        hide_min_max: true,
+        keyboard: true,
+        min: 15,
+        max: 45,
+        from: 28,
+        to: 32,
+        type: 'double',
+        step: 1,
+        grid: false
     });
 
+    $("#rangeTwo").ionRangeSlider({
+        hide_min_max: true,
+        keyboard: true,
+        min: 0,
+        max: 100,
+        from: 40,
+        to: 80,
+        type: 'double',
+        step: 1,
+        grid: false
+    });
 
- });
+    $(function() {
+        $('select').styler();
+    });
+
+    (jQuery);
+    jQuery(function(){
+        jQuery('.jq-select-multiple ul').jScrollPane({
+            verticalDragMinHeight: 20,
+            verticalDragMaxHeight: 20
+        });
+    });
+});
