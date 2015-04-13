d0e80b30a18b97486c9e58717339ae94ca3fac47
Evgenii Smirnov <knyazz@gmail.com>
2015-04-13 19:09:02 +0300

    disable vhl player parser test

--- hockeyapp/tests/test_parsers.py
+++ hockeyapp/tests/test_parsers.py
@@ -49,9 +49,9 @@ class HockeyAppParserTest(base.tests.BaseTest):
                                             #).get_page(self.mhl2_player_id)
         #self.assertIsNotNone(self.mhl2_player_data)
         # test vhl player parser
-        self.vhl_player_data = parsers.player.VHLPlayerInfo(
-                                            ).get_page(self.vhl_player_id)
-        self.assertIsNotNone(self.vhl_player_data)
+        #self.vhl_player_data = parsers.player.VHLPlayerInfo(
+                                            #).get_page(self.vhl_player_id)
+        #self.assertIsNotNone(self.vhl_player_data)
         # test mhl match parser
         self.mhl_match_data = parsers.match.HockeyMHLMatchParser(html=True,
                     absolute_url='http://mhl.khl.ru/report/272/?idgame=44367'
@@ -123,7 +123,7 @@ class HockeyAppParserTest(base.tests.BaseTest):
         for id, data in (
             (self.mhl_player_id, self.mhl_player_data),
             #(self.mhl2_player_id, self.mhl2_player_data),
-            (self.vhl_player_id, self.vhl_player_data),
+            #(self.vhl_player_id, self.vhl_player_data),
         ):
             player = Player.objects.get_or_create_player(khl_id=id,data=data)
             #check fields
