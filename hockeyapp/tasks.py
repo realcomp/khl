d58c777d32133bdc8f601ef910e51965ac23184d
Evgenii Smirnov <knyazz@gmail.com>
2015-04-15 15:15:20 +0300

    fix celery tasks

--- hockeyapp/tasks.py
+++ hockeyapp/tasks.py
@@ -172,18 +172,19 @@ def async_db_matches_update():
         Обновление инфо о матчах
     '''
     try:
-        for match in models.Match.objects.all():
-            async_db_match_update.delay(match)
+        for match_id in models.Match.objects.values_list('id', flat=True):
+            async_db_match_update.delay(match_id)
     except Exception, exc:
         logger.error(exc, exc_info=sys.exc_info())
 
 
 @app.task(ignore_result=True, track_started=True)
-def async_db_match_update(match):
+def async_db_match_update(match_id):
     b'''
         Обновление инфо о матче
     '''
     try:
+        match = models.Match.objects.get(id=match_id)
         match.count=match.count.strip(
                             ).replace(' ',''
                             ).replace('-:+',''
