58ad216316f9ce90c14cf75dfa8727546d3907dc
Okami <okami@fuzetsu.info>
2015-04-20 16:07:05 +0300

    rest 3.1.1

--- api/hockey/admin.py
+++ api/hockey/admin.py
@@ -7,10 +7,8 @@ from django.db.models import Q
 
 import rest_framework as drf
 
-from rest_framework import pagination
-
 from api.base.permissions import SportoAdminPermission
-# from api.base.paginators import AltPaginationSerializer
+from api.base.paginators import AltPagination
 from hockeyapp.models import ArenaInstaPhoto, Club, Match, Player, Arena
 
 from . import serializers
@@ -20,7 +18,7 @@ class CPAPIBase(object):
     queryset = ArenaInstaPhoto.objects.all()
     serializer_class = serializers.ArenaInstaPhotoSerializer
     permission_classes = (SportoAdminPermission,)
-    pagination_class = pagination.PageNumberPagination
+    pagination_class = AltPagination
     paginate_by = 40
 
 
