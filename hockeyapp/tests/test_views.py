bff30efe3b6f1cbe4340d87911e281359638c06d
Okami <okami@fuzetsu.info>
2015-04-20 18:11:11 +0300

    unit tests

--- hockeyapp/tests/test_views.py
+++ hockeyapp/tests/test_views.py
@@ -15,7 +15,7 @@ from ..models import Arena, Club, Player, League
 
 
 class ViewsTestCase(TestCase):
-    fixtures = 'countries',
+    fixtures = 'countries', 'leagues'
 
     PLAYER_DATA = {
         'ru_fio': 'Иванов Иван',
@@ -40,8 +40,6 @@ class ViewsTestCase(TestCase):
     }
 
     def setUp(self):
-        League.objects.create(en_title='KHL')
-
         Season.objects.create(
             start_date=datetime.date(year=2000, month=12, day=31),
             end_date=datetime.date(year=2001, month=12, day=31))
@@ -58,26 +56,24 @@ class ViewsTestCase(TestCase):
 
         self.client = Client()
 
-    def assertEqualPlayer(self, context, obj):
-        self.assertEqual(context['pk'], obj.pk)
+    def assertEqualPlayer(self, data, obj):
+        self.assertEqual(data['pk'], obj.pk)
         # TODO: set language
-        # self.assertEqual(context['fio'], obj.ru_fio)
-        self.assertEqual(context['name'], obj.ru_name)
-        self.assertEqual(context['lastname'], obj.ru_lastname)
-        # self.assertEqual(context['line'], obj.line)
-        self.assertEqual(context['line_display'], obj.get_line_display())
-        self.assertEqual(context['birth_date'], '31 December 2000')
+        self.assertEqual(data['name'], obj.ru_name)
+        self.assertEqual(data['lastname'], obj.ru_lastname)
+        # self.assertEqual(data['line'], obj.line)
+        self.assertEqual(data['line_display'], obj.get_line_display())
+        # self.assertEqual(data['birth_date'], '31 December 2000')
         delta = relativedelta.relativedelta(
             timezone.now().date(), obj.birth_date)
-        self.assertEqual(context['age'], (delta.years, delta.months))
-        self.assertEqual(context['weight'], obj.weight)
-        self.assertEqual(context['height'], obj.height)
-        self.assertEqual(context['grip'], obj.grip)
-        self.assertEqual(context['number'], obj.number)
-        self.assertEqual(
-            context['khl_url'],
-            'http://www.khl.ru/players/%s/' % obj.khl_id)
-        self.assertEqual(context['birth_date_short'], '31.12.2000')
+        self.assertEqual(data['age'], (delta.years, delta.months))
+        self.assertEqual(data['weight'], obj.weight)
+        self.assertEqual(data['height'], obj.height)
+        self.assertEqual(data['grip'], obj.grip)
+        # self.assertEqual(data['number'], obj.number)
+        # self.assertEqual(
+        #     data['khl_url'], 'http://www.khl.ru/players/%s/' % obj.khl_id)
+        self.assertEqual(data['birth_date_short'], '31.12.2000')
 
     def assertEqualClub(self, context, obj):
         self.assertEqual(context['pk'], obj.pk)
@@ -114,6 +110,12 @@ class ViewsTestCase(TestCase):
             reverse('hockeyapp:player-card', kwargs={'pk': self.player.pk}))
         self.assertEqual(response.status_code, 200)
         self.assertEqualPlayer(response.context_data, self.player)
+
+    def test_player_card_api(self):
+        response = self.client.get(
+            reverse('hockeyapp:player-card-api', kwargs={'pk': self.player.pk}))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqualPlayer(response.data, self.player)
 
     def test_player_card_indicators(self):
         response = self.client.get(
@@ -203,4 +205,19 @@ class ViewsTestCase(TestCase):
             reverse('hockeyapp:country-list-api') + '?s=Россия')
         self.assertEqual(response.data[0]['pk'], 1)
         self.assertEqual(response.data[0]['title'], 'Россия')
+        self.assertEqual(response.status_code, 200)
+
+    def test_country_league_list_api(self):
+        response = self.client.get(
+            reverse('hockeyapp:country-league-list-api'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_league_list_api(self):
+        response = self.client.get(
+            reverse('hockeyapp:league-list-api'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_player_numbers_api(self):
+        response = self.client.get(
+            reverse('hockeyapp:player-numbers-api'))
         self.assertEqual(response.status_code, 200)
