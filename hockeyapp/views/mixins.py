16930083246ef9a06e5ba89dda779dd6fbb44f8b
Okami <okami@fuzetsu.info>
2015-05-06 12:04:34 +0300

    fix #282

--- hockeyapp/views/mixins.py
+++ hockeyapp/views/mixins.py
@@ -12,14 +12,17 @@ class SeasonsMixin(object):
     def get_context_data(self, **kwargs):
         context = super(SeasonsMixin, self).get_context_data(**kwargs)
         context['request'] = self.request
+
         seasons = self.get_object().seasons
         context['seasons'] = SeasonSerializer(
             seasons, context=context, many=True).data
-        if 'season' in self.request.GET:
-            default_season = get_object_or_404(
-                Season, pk=self.request.GET['season'])
+
+        _season = self.request.GET.get('season')
+        if _season:
+            default_season = get_object_or_404(Season, pk=_season)
         else:
             default_season = seasons[0] if seasons else None
+
         context['default_season'] = SeasonSerializer(
             default_season, context=context).data
         return context
